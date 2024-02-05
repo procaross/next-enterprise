0a611b83d9f06d4eab1dd7da938fef698106a714
Proca <m2101250877@gmail.com>
2024-02-05 13:01:27 +0800

    chore: 修改import风格相关问题

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,13 @@ import withBundleAnalyzer from "@next/bundle-analyzer"
 import withPlugins from "next-compose-plugins"
 import path from "path"
 import { env } from "./env.mjs"
-
+import { fileURLToPath } from "url"
 /**
  * @type {import('next').NextConfig}
  */
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
   reactStrictMode: true,
