50f521aee9e701d2a4f6f32b80d66b8b46d37575
Proca <m2101250877@gmail.com>
2024-02-07 05:22:21 +0800

    fix: 修复中间件regex matcher引起的图片加载404

--- middleware.ts
+++ middleware.ts
@@ -102,5 +102,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|images|assets|favicon.ico|sw.js).*)"],
 }
