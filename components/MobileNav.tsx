63878297587ba51a2beac714b2067ae33bfa9be4
Proca <m2101250877@gmail.com>
2024-03-10 13:01:26 +0800

    fix: 移除featured的map方法

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -75,25 +75,6 @@ const MobileNav = () => {
                     </div>
 
                     <div className='grid grid-cols-2 gap-y-10 gap-x-4'>
-                      {category.featured.map((item) => (
-                        <div
-                          key={item.name}
-                          className='group relative text-sm'>
-                          <div className='relative aspect-square overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75'>
-                            <Image
-                              fill
-                              src={item.imageSrc}
-                              alt='product category image'
-                              className='object-cover object-center'
-                            />
-                          </div>
-                          <Link
-                            href={item.href}
-                            className='mt-6 block font-medium text-gray-900'>
-                            {item.name}
-                          </Link>
-                        </div>
-                      ))}
                     </div>
                   </li>
                 ))}
